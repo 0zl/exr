c60e4ad608342765b09b8f3ac67a291fa8ff0c85
0zl <69357965+0zl@users.noreply.github.com>
2023-10-28 21:02:01 +0000

    bruh

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('exr')
     parser.add_argument('--model', type=str, required=True)
     parser.add_argument('--max-total-token', type=int, default=4096)
-    parser.add_argument('--cache-8bit', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--print-warmup', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--cache-8bit', action='store_true', default=False)
+    parser.add_argument('--print-warmup', action='store_true', default=False)
 
     gs = parser.parse_args()
     ex = EXL(gs)
